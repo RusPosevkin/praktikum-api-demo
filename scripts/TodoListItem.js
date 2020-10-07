8eaf77d8555e584f99f94ebecd25d2504796097c
Ruslan Posevkin <rus.posevkin@gmail.com>
2020-10-08 02:45:13 +0300

    api demo

---
+++ scripts/TodoListItem.js
@@ -0,0 +1,30 @@
+class TodoListItem {
+
+  static _template = document.querySelector('#todolist-item-template').content;
+
+  constructor(data, addItem, api) {
+    this._text = data.name;
+    this._id = data.id;;
+    this._addItem = addItem;
+    this._api = api;
+  }
+
+  _delClickHandler = () => {
+    this._api.deleteTask(this._id).then(() => {
+      this._view.remove();
+    }).catch((err) => console.log(err));
+  }
+
+  _copyClickHandler = () => {
+    this._addItem(this._text);
+  }
+
+  render = (container) => {
+    this._view = TodoListItem._template.cloneNode(true).children[0];
+    this._view.querySelector('.todolist-item__text').textContent = this._text;
+    this._view.querySelector('.todolist-item__del').addEventListener('click', this._delClickHandler);
+    this._view.querySelector('.todolist-item__copy').addEventListener('click', this._copyClickHandler);
+    container.append(this._view);
+  }
+
+}
---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const api = new Api({
+  url: "https://api-test.pa7lux.ru/streams/",
+  headers: {
+		//Authorisation: 'fdsfdfsfdsa',
+    "content-type": "application/json",
+  },
+});
+
+
+const page = document.querySelector('.page');
+const createTodoListForm = (...arg) => new TodoListForm(...arg);
+const createTodoListItem = (...arg) => new TodoListItem(...arg);
+const tasks = api.getAllTasks();
+tasks.then((data) => {
+  const todoList = new TodoList(
+    data.map((item) => ({ name: item.name, id: item.id })),
+    createTodoListForm,
+    createTodoListItem,
+    api
+  );
+  todoList.render(page);
+}).catch((err) => alert(err));
